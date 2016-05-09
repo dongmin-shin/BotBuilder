0153438a4a4c8bcc2cf86e87a08264809d99b792
dong.min.shin <dong.min.shin@worksmobile.com>
2016-05-09 23:55:35 +0900

    SlackBot에 사용하려는 Token 추가

--- Node/examples/todoBot/slackBot.js
+++ Node/examples/todoBot/slackBot.js
@@ -8,8 +8,11 @@ var builder = require('../../');
 var index = require('./dialogs/index')
 
 var controller = Botkit.slackbot();
+
+var slackToken = 'xoxb-41279143126-CtiRRKjRToPxr7BajGbWzskk';
+
 var bot = controller.spawn({
-   token: process.env.token
+   token: slackToken
 });
 
 var slackBot = new builder.SlackBot(controller, bot);
--- Node/examples/todoBotLuis/slackBot.js
+++ Node/examples/todoBotLuis/slackBot.js
@@ -9,7 +9,7 @@ var index = require('./dialogs/index')
 
 var controller = Botkit.slackbot();
 var bot = controller.spawn({
-   token: process.env.token
+   token: 'xoxb-41279143126-CtiRRKjRToPxr7BajGbWzskk'
 });
 
 var slackBot = new builder.SlackBot(controller, bot);
